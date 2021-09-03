aa3b7729ac2d0e94a4ffb62fc500369371f2109f
Erich Focht <efocht@gmail.com>
2021-09-03 16:53:20 +0200

    Added script for starting container with one VE mapped.

--- README.md
+++ README.md
@@ -9,6 +9,15 @@ It does not contain any compiler!
 
 ```
 docker build --network host --tag efocht/ve-base-dev:veos2.8.3 .
+
+# tag as 'latest', if you want
+docker image tag efocht/ve-base-dev:veos2.8.3 efocht/ve-base-dev:latest
+```
+
+## Upload to dockerhub
+```
+docker push efocht/ve-base-dev:veos2.8.3
+docker push efocht/ve-base-dev:latest
 ```
 
 ## Run
---
+++ run_docker_ve.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+VENODEID=${VE_NODE_NUMBER:-0}
+VE=$(readlink -f /dev/veslot$VENODEID)
+docker run -it --rm \
+    -v $VE:$VE \
+    -v /dev:/dev:z \
+    -v /var/opt/nec/ve/veos:/var/opt/nec/ve/veos:z \
+    -v $HOME:$HOME:z \
+    -v /etc/passwd:/etc/passwd:ro \
+    -v /etc/group:/etc/group:ro \
+    -u $(id -u):$(id -g) \
+    -w $PWD \
+    efocht/ve-base-dev:latest /bin/bash
